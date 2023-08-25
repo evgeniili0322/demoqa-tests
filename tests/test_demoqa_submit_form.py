352bbb56a71d56b7c629b50088ff6a52851fa87b
Evgenii Li <evgeniili2203@gmail.com>
2023-08-25 13:14:03 +0300

    Minor fixes

--- tests/test_demoqa_submit_form.py
+++ tests/test_demoqa_submit_form.py
@@ -1,14 +1,17 @@
 import os
 from demoqa_tests.pages.registration_page import RegistrationPage
-from demoqa_tests.data.users import Student
+from demoqa_tests.data.users import User
 
 picture_path = os.path.join(os.path.dirname(__file__), 'resources', 'test.png')
-student = Student('Evgenii', 'Li', 'testacc@rambler.ru', 'Male', '7999888776', dict(month='March', day=22, year=1999),
-                  'Maths', 'Music', picture_path, 'Odesskaya, bld. 24, appt. 23', 'NCR', 'Gurgaon')
 
 
 def test_successful_submit():
+    # Arrange
     registration_page = RegistrationPage()
+
+    student = User('Evgenii', 'Li', 'testacc@rambler.ru', 'Male', '7999888776',
+                   dict(month='March', day=22, year=1999),
+                      'Maths', 'Music', picture_path, 'Odesskaya, bld. 24, appt. 23', 'NCR', 'Gurgaon')
 
     registration_page.open()
 
